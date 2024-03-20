f25a4549b4a02a5f615d408b1aaa715837505b11
Vanessa Ortega <ortegavan@live.com>
2024-03-20 16:48:33 -0300

    test: implementados testes na busca de produtos

--- modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
@@ -1,21 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+    ComponentFixture,
+    TestBed,
+    fakeAsync,
+    tick,
+} from '@angular/core/testing';
 import { ProductSearchComponent } from './product-search.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import {
+    ProductSearchService,
+    mockProducts,
+} from '@ecommerce/product-data-access';
 
 describe('ProductSearchComponent', () => {
     let component: ProductSearchComponent;
     let fixture: ComponentFixture<ProductSearchComponent>;
+    let productSearchService: ProductSearchService;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [
-                ProductSearchComponent,
-                NoopAnimationsModule,
-                HttpClientTestingModule,
+            imports: [ProductSearchComponent, NoopAnimationsModule],
+            providers: [
+                {
+                    provide: ProductSearchService,
+                    useValue: {
+                        searchByName: () => of(mockProducts),
+                    },
+                },
             ],
         }).compileComponents();
 
+        productSearchService = TestBed.inject(ProductSearchService);
         fixture = TestBed.createComponent(ProductSearchComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -23,5 +38,77 @@ describe('ProductSearchComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should debounce when input field is changed', fakeAsync(() => {
+        jest.spyOn(productSearchService, 'searchByName');
+
+        const input: HTMLInputElement =
+            fixture.nativeElement.querySelector('input');
+        input.value = 'tv';
+        input.dispatchEvent(new Event('input'));
+
+        expect(productSearchService.searchByName).not.toHaveBeenCalled();
+
+        tick(333);
+
+        expect(productSearchService.searchByName).toHaveBeenCalledWith(
+            input.value
+        );
+    }));
+
+    it('should search multiple times', fakeAsync(() => {
+        jest.spyOn(productSearchService, 'searchByName');
+        const input: HTMLInputElement =
+            fixture.nativeElement.querySelector('input');
+
+        input.value = 'tv';
+        input.dispatchEvent(new Event('input'));
+
+        tick(500);
+
+        input.value = 'notebook';
+        input.dispatchEvent(new Event('input'));
+
+        tick(500);
+
+        expect(productSearchService.searchByName).toHaveBeenCalledTimes(2);
+    }));
+
+    it('should prevent identical submissions', fakeAsync(() => {
+        jest.spyOn(productSearchService, 'searchByName');
+        const input: HTMLInputElement =
+            fixture.nativeElement.querySelector('input');
+
+        input.value = 'tv';
+        input.dispatchEvent(new Event('input'));
+
+        tick(500);
+
+        input.value = 'tv';
+        input.dispatchEvent(new Event('input'));
+
+        tick(500);
+
+        expect(productSearchService.searchByName).toHaveBeenCalledTimes(1);
+    }));
+
+    it('should prevent empty submissions', fakeAsync(() => {
+        jest.spyOn(productSearchService, 'searchByName');
+        const input: HTMLInputElement =
+            fixture.nativeElement.querySelector('input');
+
+        input.value = '';
+        input.dispatchEvent(new Event('input'));
+
+        tick(500);
+
+        expect(productSearchService.searchByName).not.toHaveBeenCalled();
+    }));
+
+    it('should return products observable correctly', () => {
+        component.products$.subscribe((products) => {
+            expect(products).toEqual(mockProducts);
+        });
     });
 });
