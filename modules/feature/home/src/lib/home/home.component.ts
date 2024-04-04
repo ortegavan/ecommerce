3f6284c9eefe1b444a99476e79a7b04cdf4c443d
Vanessa Ortega <ortegavan@live.com>
2024-04-03 23:14:39 -0300

    feat: criada lib da home

---
+++ modules/feature/home/src/lib/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'ecommerce-home',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+})
+export class HomeComponent {}
