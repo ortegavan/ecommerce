5a7a2bfe9ee8d994676bae566628df57eb46c592
Vanessa Ortega <ortegavan@live.com>
2024-04-04 01:23:30 -0300

    feat: adicionado link para home no header

--- modules/feature/layout/src/lib/layout.module.ts
+++ modules/feature/layout/src/lib/layout.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     declarations: [HeaderComponent],
     exports: [HeaderComponent],
 })
