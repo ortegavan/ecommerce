662f638d96cc7acacdd4099b1649da49182232a8
Vanessa Ortega <ortegavan@live.com>
2024-04-13 21:09:58 -0300

    feat: criada lib de detalhe do produto

---
+++ modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'ecommerce-product-detail',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './product-detail.component.html',
+    styleUrl: './product-detail.component.css',
+})
+export class ProductDetailComponent {}
