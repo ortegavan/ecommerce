85f7470d9d97c013d857a7624c0a88322bba4a9a
Vanessa Ortega <ortegavan@live.com>
2024-05-29 14:50:27 -0300

    feat: criado badge de carrinho no header do site

--- modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
+++ modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
@@ -2,19 +2,29 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { getParams } from './get-params';
 import { switchMap } from 'rxjs';
-import { ProductSearchService } from '@ecommerce/product-data-access';
+import {
+    CartService,
+    ProductSearchService,
+} from '@ecommerce/product-data-access';
 import { ProductCardComponent } from '@ecommerce/product-ui';
 import { QuantityDescriptionPipe } from '../pipes/quantity-description.pipe';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'ecommerce-product-detail',
     standalone: true,
-    imports: [CommonModule, ProductCardComponent, QuantityDescriptionPipe],
+    imports: [
+        CommonModule,
+        ProductCardComponent,
+        QuantityDescriptionPipe,
+        MatButtonModule,
+    ],
     templateUrl: './product-detail.component.html',
     styleUrl: './product-detail.component.css',
 })
 export class ProductDetailComponent {
     productSearchService = inject(ProductSearchService);
+    cartService = inject(CartService);
 
     product$ = getParams().pipe(
         switchMap((id) => this.productSearchService.getById(id))
---
+++ modules/ui/product/src/lib/components/cart/cart.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+    selector: 'ecommerce-cart',
+    standalone: true,
+    imports: [CommonModule, MatBadgeModule, MatButtonModule, MatIconModule],
+    templateUrl: './cart.component.html',
+    styleUrl: './cart.component.css',
+})
+export class CartComponent {
+    @Input() quantity = 0;
+}
