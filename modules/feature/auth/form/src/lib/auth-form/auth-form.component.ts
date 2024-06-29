1c623927cba210dfa3f4f5a0aa0fab9797ebe41b
Vanessa Ortega <ortegavan@live.com>
2024-06-29 12:04:31 -0300

    feat: criada lib para autenticação

---
+++ modules/feature/auth/form/src/lib/auth-form/auth-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'ecommerce-auth-form',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './auth-form.component.html',
+    styleUrl: './auth-form.component.css',
+})
+export class AuthFormComponent {}
